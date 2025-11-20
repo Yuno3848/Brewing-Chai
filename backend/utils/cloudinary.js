f7a72cb1aeb2215ce462803350ae64459bb168f1
Yuno3848 <bhandarianurag1234@gmail.com>
2025-11-20 22:35:59 +0530

    add cloudinary

---
+++ backend/utils/cloudinary.js
@@ -0,0 +1,37 @@
+import { v2 as cloudinary } from 'cloudinary';
+import streamifier from 'streamifier';
+import path from 'path';
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export const uploadOnCloudinary = async (fileBuffer, originalname) => {
+  try {
+    const ext = path.extname(originalname).toLowerCase(); // e.g. '.mp4', '.pdf', '.jpg'
+
+    let resourceType = 'image'; // default
+
+    if (ext === '.pdf') {
+      resourceType = 'raw';
+    } else if (['.mp4', '.mov', '.avi', '.webm', '.mkv'].includes(ext)) {
+      resourceType = 'video';
+    }
+
+    return new Promise((resolve, reject) => {
+      const stream = cloudinary.uploader.upload_stream(
+        { resource_type: resourceType },
+        (error, result) => {
+          if (error) reject(error);
+          else resolve(result);
+        },
+      );
+      streamifier.createReadStream(fileBuffer).pipe(stream);
+    });
+  } catch (error) {
+    console.error('Error while uploading file:', error);
+    return null;
+  }
+};
