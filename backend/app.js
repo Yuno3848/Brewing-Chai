ea88b2cdaa3c9e209ec29457f9228a8106aa20ef
Yuno3848 <bhandarianurag1234@gmail.com>
2025-11-20 22:33:11 +0530

    mount auth route in app

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
-import express from "express";
-import cookie from "cookie-parser";
-import cors from "cors";
+import express from 'express';
+import cookie from 'cookie-parser';
+import cors from 'cors';
+import auth from './routes/auth.route.js';
+
+const app = express();
 
 app.use(cookie());
 app.use(express.json());
@@ -9,13 +12,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
-      "http://localhost:8080",
-      "https://lms-nine-green-57.vercel.app",
+      'http://localhost:5173',
+      'http://localhost:8080',
+      'https://lms-nine-green-57.vercel.app',
     ],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
-  })
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+  }),
 );
+
+app.use('/api/brewing-chai/', auth);
 export default app;
