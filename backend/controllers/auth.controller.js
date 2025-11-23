0a9be4add5786261950b42bd4db4d04eb3652cc9
Yuno3848 <bhandarianurag1234@gmail.com>
2025-11-24 03:42:18 +0530

    update mail link, http codes and add resetPassword, verifyEmail, forgotPassword controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,3 +1,6 @@
+import mongoose from 'mongoose';
+import crypto from 'crypto';
+import cookie from 'cookie-parser';
 import User from '../models/user.model.js';
 import ApiError from '../utils/ApiError.js';
 import ApiResponse from '../utils/ApiResponse.js';
@@ -17,7 +20,7 @@ export const signUp = asyncHandler(async (req, res) => {
   });
 
   if (existingUser) {
-    throw new ApiError(400, 'User already exists');
+    throw new ApiError(409, 'User already exists');
   }
 
   let avatarData = {
@@ -41,17 +44,20 @@ export const signUp = asyncHandler(async (req, res) => {
   });
 
   if (!user) {
-    throw new ApiError(400, 'Failed to create user');
+    throw new ApiError(500, 'Failed to create user');
   }
 
-  const otp = user.generateTemporaryToken();
+  const { unhashedToken, hashedToken, tokenExpiry } = user.generateTemporaryToken();
 
+  user.emailVerificationToken = hashedToken;
+  user.emailVerificationTokenExpiry = tokenExpiry;
+
+  await user.save({ validateBeforeSave: false });
   await sendMail({
     senderEmail: user.email,
     subject: 'Email Verification',
-    text: `Verification OTP: ${otp}`,
-
-    html: `<p>Your OTP is <strong>${otp}</strong></p>`,
+    text: `Verification Link:  http://localhost:5173/verify-email/${unhashedToken}`,
+    html: `<p>Your Verification Link is  http://localhost:5173/verify-email/${unhashedToken}</p>`,
   });
 
   const createdUser = await User.findById(
@@ -61,10 +67,191 @@ export const signUp = asyncHandler(async (req, res) => {
       otp: 0,
       otpExpiry: 0,
       forgotPasswordExpiry: 0,
-      isEmailVerified: 0,
+      emailVerificationToken: 0,
+      emailVerificationTokenExpiry: 0,
     },
     { lean: true },
   );
 
   res.status(201).json(new ApiResponse(201, 'User registered successfully', createdUser));
+});
+
+export const verifyEmail = asyncHandler(async (req, res) => {
+  const { token } = req.params;
+
+  const emailVerificationToken = crypto.createHash('sha256').update(token).digest('hex');
+
+  const user = await User.findOne({
+    emailVerificationToken,
+    emailVerificationTokenExpiry: { $gt: Date.now() },
+  });
+
+  if (!user) {
+    throw new ApiError(404, 'User not found or token is invalid');
+  }
+
+  if (user.isEmailVerified) {
+    return res.status(200).json(
+      new ApiResponse(200, 'Email is already verified', {
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+        isEmailVerified: true,
+      }),
+    );
+  }
+
+  user.isEmailVerified = true;
+  user.emailVerificationToken = undefined;
+  user.emailVerificationTokenExpiry = undefined;
+
+  await user.save();
+
+  res.status(200).json(new ApiResponse(200, 'Email verified successfully', user));
+});
+
+export const login = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new ApiError(404, 'Invalid email or password!');
+  }
+  if (!user.isEmailVerified) {
+    throw new ApiError(404, 'Email is not verified!');
+  }
+
+  const isValidPassword = await user.comparePassword(password);
+
+  if (!isValidPassword) {
+    throw new ApiError(401, 'Invalid Password');
+  }
+
+  const cookiesOptions = {
+    secure: false,
+    httpOnly: true,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    sameSite: 'lax',
+  };
+
+  const accessToken = await user.generateAccessToken();
+
+  return res
+    .status(200)
+    .cookie('accessToken', accessToken, cookiesOptions)
+    .json(new ApiResponse(200, 'User logged in successfully'));
+});
+
+export const logOut = asyncHandler(async (req, res) => {
+  const userId = req.user.id;
+
+  if (!userId) {
+    throw new ApiError(401, 'Unauthorize Access!');
+  }
+  const cookiesOptions = {
+    secure: false,
+    httpOnly: true,
+    sameSite: 'lax',
+  };
+
+  return res
+    .status(200)
+    .clearCookie('accessToken', cookiesOptions)
+    .json(new ApiResponse(200, 'User logged out!'));
+});
+
+export const forgotPassword = asyncHandler(async (req, res) => {
+  const { email } = req.body;
+
+  const user = await User.findOne({ email }).select(
+    '-emailVerifiedToken -emailVerificationTokenExpiry',
+  );
+  if (!user) {
+    throw new ApiError(400, `User not found`);
+  }
+
+  const { unhashedToken, hashedToken, tokenExpiry } = user.generateTemporaryToken();
+
+  user.forgotPasswordToken = hashedToken;
+  user.forgotPasswordExpiry = tokenExpiry;
+
+  await user.save({ validateBeforeSave: false });
+
+  await sendMail({
+    senderEmail: user.email,
+    subject: 'Reset Password',
+    text: `Reset password link : http://localhost:5173/reset-password/${unhashedToken}`,
+
+    html: `Reset password link : http://localhost:5173/reset-password/${unhashedToken}`,
+  });
+
+  return res.status(200).json(
+    new ApiResponse(200, 'Password forgot successfully', {
+      _id: user.id,
+      email: user.email,
+      password: user.password,
+      username: user.username,
+      isEmailVerified: user.isEmailVerified,
+    }),
+  );
+});
+
+export const resetPassword = asyncHandler(async (req, res) => {
+  const { token } = req.params;
+  console.log('token :', token);
+  const { newPassword, confirmPassword } = req.body;
+
+  if (newPassword != confirmPassword) {
+    throw new ApiError(400, 'Password and confirm password does not match!');
+  }
+
+  const forgotPasswordToken = crypto.createHash('sha256').update(token).digest('hex');
+
+  const user = await User.findOne({
+    forgotPasswordToken,
+    forgotPasswordExpiry: { $gt: Date.now() },
+  });
+
+  if (!user) {
+    throw new ApiError(404, 'Invalid token...');
+  }
+
+  user.password = newPassword;
+  user.forgotPasswordToken = undefined;
+  user.forgotPasswordExpiry = undefined;
+
+  await user.save();
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, 'password reset successfully. Please sign in.', user));
+});
+
+export const resendVerifyEmail = asyncHandler(async (req, res) => {
+  const { email } = req.body;
+
+  const user = await User.findOne({ email }).select(
+    '-forgotPasswordToken -forgotPasswordTokenExpiry',
+  );
+  if (!user) {
+    throw new ApiError(400, `Invalid email!`);
+  }
+
+  const { unhashedToken, hashedToken, tokenExpiry } = user.generateTemporaryToken();
+
+  user.emailVerificationToken = hashedToken;
+  user.emailVerificationTokenExpiry = tokenExpiry;
+
+  await user.save({ validateBeforeSave: false });
+
+  await sendMail({
+    senderEmail: user.email,
+    subject: 'Email verification',
+    text: `Email verification link : http://localhost:5173/verify-email/${unhashedToken}`,
+
+    html: `Email verification link : http://localhost:5173/verify-email/${unhashedToken}`,
+  });
+
+  return res.status(200).json(new ApiResponse(200, 'Email verification sent successfully!', user));
 });
