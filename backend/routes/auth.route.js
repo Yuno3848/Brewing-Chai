324089f502532510e7850f102730d29686b214c0
Yuno3848 <bhandarianurag1234@gmail.com>
2025-11-24 03:43:11 +0530

    add auth routes

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,10 +1,27 @@
 import { Router } from 'express';
 import validate from '../middleware/validate.middleware.js';
-import { signUpSchema } from '../validators/user.validator.js';
-import { signUp } from '../controllers/auth.controller.js';
+import {
+  signIn,
+  signUpSchema,
+  verify,
+  validateResetPassword,
+  validateEmail,
+} from '../validators/user.validator.js';
+import {
+  forgotPassword,
+  login,
+  resendVerifyEmail,
+  resetPassword,
+  signUp,
+  verifyEmail,
+} from '../controllers/auth.controller.js';
 import { uploadMul } from '../middleware/multer.middleware.js';
 const auth = Router();
 
 auth.post('/signUp', uploadMul.single('avatar'), validate(signUpSchema), signUp);
-
+auth.patch('/verify-email/:token', validate(verify), verifyEmail);
+auth.post('/signIn', validate(signIn), login);
+auth.post('/forgot-password', validate(validateEmail), forgotPassword);
+auth.patch('/reset-password/:token', validate(validateResetPassword), resetPassword);
+auth.patch('/resend-verify-email', validate(validateEmail), resendVerifyEmail);
 export default auth;
