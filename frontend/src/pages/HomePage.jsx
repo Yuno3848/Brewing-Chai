5589bbab19eec258b5bdebf20680e217ad0ee71e
Yuno3848 <bhandarianurag1234@gmail.com>
2025-11-24 03:51:36 +0530

    create HomePage

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Home from '../features/home/ChaiDemyHome'
+
+const HomePage = () => {
+  return (
+    <div>
+      <Home/>
+    </div>
+  )
+}
+
+export default HomePage
