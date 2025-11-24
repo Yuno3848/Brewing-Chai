b8018f7f68c5e83e8c4f5f4ec4ee4816c88416e5
Yuno3848 <bhandarianurag1234@gmail.com>
2025-11-24 09:47:36 +0530

    add check-email path

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -7,6 +7,7 @@ import ResetPasswordPage from "./pages/ResetPasswordPage";
 import VerifyPage from "./pages/VerifyPage";
 import HomePage from "./pages/HomePage";
 import Layout from "./components/layout/Layout";
+import CheckMailPage from "./pages/CheckMailPage";
 
 const Routes = createBrowserRouter([
   {
@@ -20,13 +21,13 @@ const Routes = createBrowserRouter([
     ],
   },
 
-  //public apis
+  //public routes
   { path: "/signin", element: <SigninPage /> },
   { path: "/signup", element: <SignupPage /> },
   { path: "/forgot-password", element: <ForgotPasswordPage /> },
   { path: "/reset-password/:token", element: <ResetPasswordPage /> },
   { path: "/verify-email/:token", element: <VerifyPage /> },
-  { path: "/", element: <HomePage /> },
+  { path: "/check-email", element: <CheckMailPage /> },
 ]);
 
 export default Routes;
