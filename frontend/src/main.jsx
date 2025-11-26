c23fd5d711ab1aabc3602afd8773b2d914f84be7
Yuno3848 <bhandarianurag1234@gmail.com>
2025-11-26 12:38:25 +0530

    chore: add Provider and Toaster wrapper in root render

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import {Toaster} from "react-hot-toast"
+import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App.jsx";
+import store from "./redux/store.js";
 
 createRoot(document.getElementById("root")).render(
   <>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
+
     <Toaster position="top-right" reverseOrder={false} />
   </>
 );
