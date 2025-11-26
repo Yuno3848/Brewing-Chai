8a3bdbc60d4afef4932aab5f887d219ba1807a56
Yuno3848 <bhandarianurag1234@gmail.com>
2025-11-26 14:21:49 +0530

    feat: implement async fetchMe action in auth slice using createAsyncThunk

--- frontend/src/redux/slices/auth.slice.js
+++ frontend/src/redux/slices/auth.slice.js
@@ -1,9 +1,24 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import auth from "../../features/auth/api/auth.api";
 
 const initialState = {
   user: null,
   isAuthenticated: false,
+  isLoading: false,
+  isError: false,
 };
+
+export const fetchMe = createAsyncThunk(
+  "auth/fetchMe",
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await auth.me();
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error || "failed to fetch user!");
+    }
+  }
+);
 
 const authSlice = createSlice({
   name: "auth",
@@ -15,6 +30,25 @@ const authSlice = createSlice({
     },
 
     logout: () => ({ ...initialState }),
+  },
+
+  extraReducers: (builder) => {
+    builder.addCase(fetchMe.fulfilled, (state, action) => {
+      state.user = action.payload;
+      state.isAuthenticated = true;
+      state.isLoading = false;
+    });
+
+    builder.addCase(fetchMe.pending, (state, action) => {
+      state.isLoading = true;
+    });
+
+    builder.addCase(fetchMe.rejected, (state, action) => {
+      console.log("fetch error :", action.payload);
+      state.isError = true;
+      state.isLoading = false;
+      state.user = null;
+    });
   },
 });
 
