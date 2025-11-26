695fc88ad50994c04ebea6f7ce6c5588d9605f2c
Yuno3848 <bhandarianurag1234@gmail.com>
2025-11-26 12:37:09 +0530

    feat : mount /me route

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -10,12 +10,14 @@ import {
 import {
   forgotPassword,
   login,
+  me,
   resendVerifyEmail,
   resetPassword,
   signUp,
   verifyEmail,
 } from '../controllers/auth.controller.js';
 import { uploadMul } from '../middleware/multer.middleware.js';
+import isLogged from '../middleware/isLogged.middleware.js';
 const auth = Router();
 
 auth.post('/signUp', uploadMul.single('avatar'), validate(signUpSchema), signUp);
@@ -24,4 +26,5 @@ auth.post('/signIn', validate(signIn), login);
 auth.post('/forgot-password', validate(validateEmail), forgotPassword);
 auth.patch('/reset-password/:token', validate(validateResetPassword), resetPassword);
 auth.patch('/resend-verify-email', validate(validateEmail), resendVerifyEmail);
+auth.get("/me", isLogged,me )
 export default auth;
