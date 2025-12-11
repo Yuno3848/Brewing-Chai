8d0e53831e300c669a13107728ad2cdb5ed932b2
Yuno3848 <bhandarianurag1234@gmail.com>
2025-12-11 19:09:00 +0530

    feat : add public profile page

---
+++ frontend/src/pages/profile/PublicProfilePage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import PublicProfile from "../../features/profiles/pages/PublicProfile";
+
+const PublicProfilePage = () => {
+  return <PublicProfile />;
+};
+
+export default PublicProfilePage;
