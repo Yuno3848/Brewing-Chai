cca788a9d4bf169764ef39b20d8cab809ae78e05
Yuno3848 <bhandarianurag1234@gmail.com>
2025-12-11 18:32:05 +0530

    feat : Add AccountSecurityPage

---
+++ frontend/src/pages/profile/AccountPrivacyPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import AccountSecurity from "../../features/profiles/pages/AccountSecurity";
+
+const AccountSecurityPage = () => {
+  return <AccountSecurity />;
+};
+
+export default AccountSecurityPage;
