f777fd471a1a8e065567d55403aa9e0da4aa870e
Yuno3848 <bhandarianurag1234@gmail.com>
2025-12-11 18:32:22 +0530

    feat : Add DeleteAccountPage

---
+++ frontend/src/pages/profile/DeleteAccountPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import DeleteAccount from '../../features/profiles/pages/DeleteAccount'
+
+const DeleteAccountPage = () => {
+  return (
+ <DeleteAccount/>
+  )
+}
+
+export default DeleteAccountPage
