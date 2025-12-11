5cfc698b58e2fe04c13801bdb46d1c3cbf648a10
Yuno3848 <bhandarianurag1234@gmail.com>
2025-12-11 18:35:15 +0530

    feat : Add CheckEmail Page

---
+++ frontend/src/pages/auth/CheckMailPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import CheckEmail from "../../features/auth/pages/CheckEmail";
+
+const CheckMailPage = () => {
+  return <CheckEmail />;
+};
+
+export default CheckMailPage;
