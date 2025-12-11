b25360acb596998637b6725781c5783bc0875e5d
Yuno3848 <bhandarianurag1234@gmail.com>
2025-12-11 18:32:57 +0530

    feat : Add NotificationPage

---
+++ frontend/src/pages/profile/NotificationPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Notification from "../../features/profiles/pages/Notification";
+
+const NotificationPage = () => {
+  return <Notification />;
+};
+
+export default NotificationPage;
