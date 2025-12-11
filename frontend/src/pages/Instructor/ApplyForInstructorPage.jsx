e3bdaaee1e702239be392d6f3abd72ed534c4cd0
Yuno3848 <bhandarianurag1234@gmail.com>
2025-12-11 19:20:48 +0530

    feat : ApplyForInstructor page

---
+++ frontend/src/pages/Instructor/ApplyForInstructorPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ApplyForInstructor from '../../features/instructor/pages/ApplyForInstructor'
+
+const ApplyForInstructorPage = () => {
+  return (
+   <ApplyForInstructor/>
+  )
+}
+
+export default ApplyForInstructorPage
