26fa70c02c2c8d9d259694ae89032fc089a325a3
Yuno3848 <bhandarianurag1234@gmail.com>
2025-12-11 18:43:54 +0530

    feat : add InstructorLayout

---
+++ frontend/src/features/instructor/component/InstructorLayout.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import SidebarInstructor from "./SidebarInstructor";
+import { Outlet } from "react-router";
+
+const InstructorLayout = () => {
+  return (
+    <div className="flex min-h-screen bg-[#E7D7C5] text-[#5D4037] font-sans items-center">
+      <div className="w-[320px] bg-[#F4E8D6] border-r border-[#D0BFAF] p-6 shadow-xl rounded-tr-3xl rounded-br-3xl flex flex-col mt-5 mb-5">
+        <SidebarInstructor />
+      </div>
+
+      <div className="flex-1 p-8">
+        <div className="bg-[#FFF8F3] shadow-md rounded-3xl p-8 min-h-[calc(100vh-4rem)] flex justify-center items-center">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InstructorLayout;
