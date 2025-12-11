2690bd0013059b8c16b414844af8be40bc9eea9b
Yuno3848 <bhandarianurag1234@gmail.com>
2025-12-11 19:20:27 +0530

    feat : add instructor profile page

---
+++ frontend/src/pages/Instructor/InstructorProfilePage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import InstructorProfile from "../../features/instructor/pages/InstructorProfile";
+
+const InstructorProfilePage = () => {
+  return;
+  <InstructorProfile />;
+};
+
+export default InstructorProfilePage;
