831a19a5ff1c203ba78a49cfe07cb1b26b67c44b
Yuno3848 <bhandarianurag1234@gmail.com>
2025-12-11 18:32:42 +0530

    feat : Add EditAvatarPage

---
+++ frontend/src/pages/profile/EditAvatarPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import EditAvatar from '../../features/profiles/pages/EditAvatar'
+
+const EditAvatarPage = () => {
+  return (
+   <EditAvatar/>
+  )
+}
+
+export default EditAvatarPage
